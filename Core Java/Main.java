bbb21ea41cb55eb5eba9e83491b7b732d402dbe5
Rishabh Kumar <rsk1998200@gmail.com>
2025-11-23 04:33:56 +0530

    Notes and first program

---
+++ Core Java/Main.java
@@ -0,0 +1,11 @@
+// To run this code, first compile the code using:
+// - javac {path}Main.java or make the current dir as the path in which the file is present, then write javac Main.java
+
+// Then run the compiled code using:
+// - java {Path}Main or make the current dir as the path in which the file is present, then write java Main {class name}
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+}
