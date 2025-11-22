15026a9e9839e6b2fe74b2871c3e1f26099dda3e
Rishabh Kumar <rsk1998200@gmail.com>
2025-11-23 04:59:07 +0530

    Datatypes

--- Core Java/Main.java
+++ Core Java/Main.java
@@ -7,5 +7,27 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!!");
+
+        // Creating Variables 
+        int num1 = 10; // (Store integer values)
+        int num2 = 20; // (Store integer values)
+        int result = num1 + num2; // Addition of two integers
+
+        System.out.println(result);
+
+        // Data-types in Java
+        byte age = 25;
+        float marksFloat = 6.5F;
+        long largeNumber = 123456789L;
+        double marksDouble = 8.75;
+        char grade = 'A';
+        boolean isPassed = true;
+
+        System.out.println("Age: " + age);
+        System.out.println("Marks (float): " + marksFloat);
+        System.out.println("Marks (double): " + marksDouble);
+        System.out.println("Grade: " + grade);
+        System.out.println("Passed: " + isPassed);
+        System.out.println("Large Number: " + largeNumber);
     }
 }
